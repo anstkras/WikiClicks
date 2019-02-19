7523b6cff3a5471fb29032c04e5c7be00c2467d0
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-02-19 15:36:55 +0300

    add main controller

---
+++ app/src/main/java/ru/hse/wikiclicks/controllers/MainController.java
@@ -0,0 +1,8 @@
+package ru.hse.wikiclicks.controllers;
+
+
+public class MainController {
+    public static String getRandomPageLink() {
+        return "https://en.wikipedia.org/wiki/Avatar_(band)";
+    }
+}
