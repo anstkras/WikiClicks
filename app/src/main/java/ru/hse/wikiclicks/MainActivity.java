1f9878ffc38894559dc3c24027b9a443f37db2d0
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-02-19 14:15:54 +0300

    add webwiew

--- app/src/main/java/ru/hse/wikiclicks/MainActivity.java
+++ app/src/main/java/ru/hse/wikiclicks/MainActivity.java
@@ -2,6 +2,8 @@ package ru.hse.wikiclicks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new Browser());
+        myWebView.loadUrl("http://www.wikipedia.com"); // далеe хочется получать html от main controller
+    }
+
+    private class Browser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
