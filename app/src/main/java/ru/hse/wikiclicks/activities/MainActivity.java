e98ef0cba150d764601484cb6b612971f7edba8f
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-02-19 15:27:00 +0300

    override onBackPressed to go back in web view
    add main controller

--- app/src/main/java/ru/hse/wikiclicks/activities/MainActivity.java
+++ app/src/main/java/ru/hse/wikiclicks/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package ru.hse.wikiclicks;
+package ru.hse.wikiclicks.activities;
 
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
@@ -8,20 +8,33 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
+import ru.hse.wikiclicks.R;
+import ru.hse.wikiclicks.controllers.MainController;
+
 public class MainActivity extends AppCompatActivity {
     private int stepsCount = -2; // magic
     private TextView stepsTextView;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView myWebView = findViewById(R.id.webview);
-        myWebView.setWebViewClient(new WikiWebViewClient());
-        myWebView.loadUrl("https://en.wikipedia.org/wiki/Avatar_(band)");
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WikiWebViewClient());
+        webView.loadUrl(MainController.getRandomPageLink());
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         stepsTextView = findViewById(R.id.stepsTextView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private class WikiWebViewClient extends WebViewClient {
