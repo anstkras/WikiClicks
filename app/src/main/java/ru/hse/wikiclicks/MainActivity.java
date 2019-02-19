8d044e389fd37914f3213c3de7cacdf667cf7387
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-02-19 15:22:06 +0300

    add steps counting

--- app/src/main/java/ru/hse/wikiclicks/MainActivity.java
+++ app/src/main/java/ru/hse/wikiclicks/MainActivity.java
@@ -1,26 +1,39 @@
 package ru.hse.wikiclicks;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int stepsCount = -2; // magic
+    private TextView stepsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView myWebView = findViewById(R.id.webview);
-        myWebView.setWebViewClient(new Browser());
-        myWebView.loadUrl("http://www.wikipedia.com"); // далеe хочется получать html от main controller
+        myWebView.setWebViewClient(new WikiWebViewClient());
+        myWebView.loadUrl("https://en.wikipedia.org/wiki/Avatar_(band)");
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        stepsTextView = findViewById(R.id.stepsTextView);
     }
 
-    private class Browser extends WebViewClient {
+    private class WikiWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            stepsCount++;
+            stepsTextView.setText("Steps: " + stepsCount);
         }
     }
 }
