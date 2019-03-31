ebd34e8a2c685b663665ffa57670453efb6c8266
crossopt <s17b2_fedorkina@179.ru>
2019-03-31 21:05:29 +0300

    fixed random

--- app/src/main/java/ru/hse/wikiclicks/controllers/MainController.java
+++ app/src/main/java/ru/hse/wikiclicks/controllers/MainController.java
@@ -3,6 +3,6 @@ package ru.hse.wikiclicks.controllers;
 
 public class MainController {
     public static String getRandomPageLink() {
-        return "https://en.wikipedia.org/wiki/Avatar_(band)";
+        return "https://en.wikipedia.org/wiki/Special:Random";
     }
 }
