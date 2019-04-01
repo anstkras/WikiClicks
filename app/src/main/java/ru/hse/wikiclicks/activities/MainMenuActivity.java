567e99e3b5e014898bc6bf216ddbe839ab86c6b1
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-04-01 23:29:50 +0300

    add set up for exit button

--- app/src/main/java/ru/hse/wikiclicks/activities/MainMenuActivity.java
+++ app/src/main/java/ru/hse/wikiclicks/activities/MainMenuActivity.java
@@ -1,13 +1,16 @@
 package ru.hse.wikiclicks.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 
 import ru.hse.wikiclicks.R;
+
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
@@ -21,6 +24,34 @@ public class MainMenuActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent startGame = new Intent(MainMenuActivity.this, MainActivity.class);
                 startActivity(startGame);
+            }
+        });
+
+        setUpExitButton();
+    }
+
+    private void setUpExitButton() {
+        Button exitButton = findViewById(R.id.ExitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainMenuActivity.this);
+                builder.setTitle("Exit");
+                builder.setMessage("Are you sure you want to exit?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
