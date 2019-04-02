e4b6bc94c240343b7057cc427d0e83975134ae80
crossopt <s17b2_fedorkina@179.ru>
2019-04-02 20:35:13 +0300

    improved search

--- app/src/main/java/ru/hse/wikiclicks/controllers/MainController.java
+++ app/src/main/java/ru/hse/wikiclicks/controllers/MainController.java
@@ -44,12 +44,12 @@ public class MainController {
     }
 
     public static List<WikiPage> getSearchSuggestions(String prefix) {
-        String query = "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=30&srsearch=" + prefix;
+        String query = "https://en.wikipedia.org/w/api.php?action=query&list=prefixsearch&prop=info&inprop=url&utf8=&format=json&origin=*&pslimit=15&pssearch=" + prefix;
         try {
             String searchResult = Jsoup.connect(query).timeout(0).ignoreContentType(true).execute().body();
             System.out.println(searchResult);
             JSONObject json = new JSONObject(searchResult);
-            JSONArray results = json.getJSONObject("query").getJSONArray("search");
+            JSONArray results = json.getJSONObject("query").getJSONArray("prefixsearch");
             ArrayList<WikiPage> suggestions = new ArrayList<>();
             for (int i = 0; i < results.length(); ++i) {
                 String page_id = results.getJSONObject(i).getString("pageid");
@@ -65,5 +65,39 @@ public class MainController {
             e.printStackTrace();
         }
         return new ArrayList<>();
+    }
+
+    public static String getURLForId(String id) {
+        String query = "https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&format=json&pageids=" + id;
+        try {
+            String searchResult = Jsoup.connect(query).timeout(0).ignoreContentType(true).execute().body();
+            System.out.println(searchResult);
+            JSONObject json = new JSONObject(searchResult);
+            String desktopURL = json.getJSONObject("query").getJSONObject("pages").getJSONObject(id).getString("canonicalurl");
+            return desktopURL.replace("en.wikipedia", "en.m.wikipedia");
+        } catch (IOException e) {
+            System.out.println("Internet connection failed.");
+            e.printStackTrace();
+        } catch (JSONException e) {
+            System.out.println("JSON failed");
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String getExtract(String id) {
+        String query = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exlimit=1&exsentences=3&continue=&explaintext=1&format=json&formatversion=2&pageids=" + id;
+        try {
+            String searchResult = Jsoup.connect(query).timeout(0).ignoreContentType(true).execute().body();
+            JSONObject json = new JSONObject(searchResult);
+            return json.getJSONObject("query").getJSONArray("pages").getJSONObject(0).getString("extract");
+        } catch (IOException e) {
+            System.out.println("Internet connection failed.");
+            e.printStackTrace();
+        } catch (JSONException e) {
+            System.out.println("JSON failed");
+            e.printStackTrace();
+        }
+        return "";
     }
 }
