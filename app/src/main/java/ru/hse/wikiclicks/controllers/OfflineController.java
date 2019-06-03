0dc1888d76495b9796a1827472aa6c0cd383c8b6
crossopt <s17b2_fedorkina@179.ru>
2019-06-03 23:31:50 +0300

    a proper failure

--- app/src/main/java/ru/hse/wikiclicks/controllers/OfflineController.java
+++ app/src/main/java/ru/hse/wikiclicks/controllers/OfflineController.java
@@ -1,5 +1,7 @@
 package ru.hse.wikiclicks.controllers;
 
+import android.util.Log;
+
 import org.jsoup.Connection;
 import java.io.File;
 import java.io.IOException;
@@ -12,26 +14,34 @@ public class OfflineController {
 
     public static void downloadTree(ChooseOfflineGame game, String outputDirectory, int confirmationNumber) {
         for (String title : game.getPages()) {
-            downloadPage(title, outputDirectory);
+            try {
+                downloadPage(title, outputDirectory);
+            } catch (IOException first) {
+                try {
+                    Thread.sleep(1000);
+                    downloadPage(title, outputDirectory);
+                } catch (InterruptedException | IOException second) {
+                    // well, it really doesn't work. fail without confirmation.
+                    Log.e("Failed page download", title);
+                    return;
+                }
+            }
         }
         downloadConfirmation(outputDirectory, confirmationNumber);
     }
 
-    private static void downloadPage(String title, String outputDirectory) {
-        try {
-            final File file = new File(outputDirectory, normalize(title));
-            if (file.exists()) {
-                return; //assume page already is downloaded
-            }
-            String url = "https://en.wikipedia.org/wiki/" + title;
-            final Connection.Response response = Jsoup.connect(url).timeout(0).ignoreContentType(true).execute();
-            String webPage = response.parse().html();
-            //dirty fix for links, inter-page ones might not work
-            webPage = webPage.replaceAll("href=\"", "href=\"" + "https://");
-            FileUtils.writeStringToFile(file, webPage, "UTF-8");
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static void downloadPage(String title, String outputDirectory) throws IOException {
+        final File file = new File(outputDirectory, normalize(title));
+        if (file.exists()) {
+            return; //assume page already is downloaded
         }
+        String url = "https://en.wikipedia.org/wiki/" + title;
+        final Connection.Response response = Jsoup.connect(url).timeout(0).ignoreContentType(true).execute();
+        String webPage = response.parse().html();
+        //dirty fix for links, inter-page ones might not work
+        webPage = webPage.replaceAll("href=\"", "href=\"" + "https://");
+        FileUtils.writeStringToFile(file, webPage, "UTF-8");
+
     }
 
     public static String readPage(String title, String inputDirectory) throws IOException {
@@ -47,8 +57,7 @@ public class OfflineController {
         System.out.println(file.getAbsolutePath());
         try {
             FileUtils.writeStringToFile(file, "downloaded successfully", "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ignored) { //lack of confirmation will show that failure happened.
         }
     }
 }
