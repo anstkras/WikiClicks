130177ff5f4facda286ca7c95419d8fd086595dc
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-06-08 11:08:19 +0300

    change successful sign in message

--- app/src/main/java/ru/hse/wikiclicks/activities/ChallengesActivity.java
+++ app/src/main/java/ru/hse/wikiclicks/activities/ChallengesActivity.java
@@ -143,7 +143,7 @@ public class ChallengesActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Toast toast = Toast.makeText(this, "sing in successful", Toast.LENGTH_LONG);
+            Toast toast = Toast.makeText(this, "sign in successful", Toast.LENGTH_LONG);
             toast.show();
             signInButton.setVisibility(View.GONE);
             signOutButton.setVisibility(View.VISIBLE);
